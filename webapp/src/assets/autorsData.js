93bc258b6c0495d9f685f287909e0b002a57c25a
Angie Moya <angiemoyau98@gmail.com>
2022-11-23 18:50:12 -0500

    iniciando autors

---
+++ webapp/src/assets/autorsData.js
@@ -0,0 +1,30 @@
+const autors = [
+    {
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRB88Z0Z6DCxDbC927-XtSLe_jEzdT3xa4oVw&usqp=CAU",
+        name: "Madonna",
+        songsNum: "12",
+    },
+    {
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqw8oxfbGEwI8mCtgMu6BFx-EUasiw5xPQXA&usqp=CAU",
+        name: "Maluma",
+        songsNum: "13",
+    },
+    {
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQaQ35CsYOmS-6zkH3IxvDtNkEH12aG-hHtcw&usqp=CAU",
+        name: "Alejandro Sanz",
+        songsNum: "7",
+    },
+    {
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-EV-uaUNmItlVEfs4Cufn2XPoZ-RPwYFTEA&usqp=CAU",
+        name: "Ariana Grande",
+        songsNum: "9",
+    },
+    {
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQlVkhsS264Xhtl_aIfnrA6BkNfzfcObJwiaw&usqp=CAU",
+        name: "Shakira",
+        songsNum: "10",
+    },
+
+]
+
+export default autors;
--- webapp/src/components/Autors.js
+++ webapp/src/components/Autors.js
@@ -1,9 +1,27 @@
 import React from "react";
+import ListGroup from "react-bootstrap/ListGroup";
+import data from "../assets/autorsData";
+import "../styles/Autors.css";
 
-function Autors(){
-    return(
-        <div className="Autors">Autors</div>
-    )
+function Autors() {
+  return (
+    <div className="Autors">
+      <h2>Autores</h2>
+      <div className="autorslist">
+        <ListGroup>
+            {data && data.map((autor) => (
+                <ListGroup.Item>
+                    <div>
+                        <img src={autor.image}></img>
+                        <h3><em>{autor.name}</em></h3>
+                        <p>{autor.songsNum}</p>
+                    </div>
+                </ListGroup.Item>
+            ))}
+        </ListGroup>
+      </div>
+    </div>
+  );
 }
 
 export default Autors;
