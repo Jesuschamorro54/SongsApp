beb9993826517a013e1901ed043c5cc7a533bcc9
Angie Moya <angiemoyau98@gmail.com>
2022-11-23 18:09:44 -0500

    detalles agregados a vista canciones

--- webapp/src/components/Songs.js
+++ webapp/src/components/Songs.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 import SongCard from "./SongCard";
 import songs from "../assets/data";
 import "../styles/Songs.css";
@@ -7,6 +9,15 @@ function Songs() {
   return (
     <div className="Songs">
       <h2>Biblioteca de canciones</h2>
+      <Form className="d-flex SongsSearch">
+          <Form.Control
+            type="search"
+            placeholder="Search"
+            className="me-2"
+            aria-label="Search"
+          />
+          <Button className="btn-search">Search</Button>
+        </Form>
       <SongCard data={songs} />
     </div>
   );
