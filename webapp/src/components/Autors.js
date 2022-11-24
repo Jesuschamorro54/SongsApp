70bd85644b011aab0f09798b26089ee8c9e6dcad
Angie Moya <angiemoyau98@gmail.com>
2022-11-24 13:02:24 -0500

    finalizada vista de autores

--- webapp/src/components/Autors.js
+++ webapp/src/components/Autors.js
@@ -1,22 +1,41 @@
 import React from "react";
 import ListGroup from "react-bootstrap/ListGroup";
+import Dropdown from "react-bootstrap/Dropdown";
 import data from "../assets/autorsData";
 import "../styles/Autors.css";
 
 function Autors() {
   return (
     <div className="Autors">
-      <h2>Autores</h2>
+      <h2>Lista de Autores</h2>
+      <div className="filter">
+        <Dropdown>
+          <Dropdown.Toggle variant="info" id="dropdown-basic">
+            Filtrar por
+          </Dropdown.Toggle>
+          <Dropdown.Menu>
+            <Dropdown.Item href="#/action-1">Orden alfabético</Dropdown.Item>
+            <Dropdown.Item href="#/action-2">
+              Mayor número de canciones agregadas
+            </Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      </div>
       <div className="autorslist">
         <ListGroup>
-            {data && data.map((autor) => (
-                <ListGroup.Item>
-                    <div>
-                        <img src={autor.image}></img>
-                        <h3><em>{autor.name}</em></h3>
-                        <p>{autor.songsNum}</p>
-                    </div>
-                </ListGroup.Item>
+          {data &&
+            data.map((autor) => (
+              <ListGroup.Item>
+                <div className="container-list">
+                  <img src={autor.image}></img>
+                  <div className="description">
+                    <h3>
+                      <em>{autor.name}</em>
+                    </h3>
+                    <p>Canciones agregadas: {autor.songsNum}</p>
+                  </div>
+                </div>
+              </ListGroup.Item>
             ))}
         </ListGroup>
       </div>
