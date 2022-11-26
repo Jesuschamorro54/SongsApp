ba8de4d2af83902902605b508f0a1baaf5bca57e
Jesus chamorro <jesusfeli54@gmail.com>
2022-11-26 15:00:02 -0500

    configuracion de CORDS en la API

--- app/src/__init__.py
+++ app/src/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
 
 from .routes import api
 
 def create_app():
     app = Flask(__name__)
+    cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
     
     # Registro de los Blueprints
     app.register_blueprint(api)
