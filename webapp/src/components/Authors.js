335530913064c87dc8d2443c1b6f9afa74926e4c
Angie Moya <angiemoyau98@gmail.com>
2022-11-27 21:07:00 -0500

    finalizando detalles

--- webapp/src/components/Authors.js
+++ webapp/src/components/Authors.js
@@ -1,11 +1,15 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import ListGroup from "react-bootstrap/ListGroup";
-import Dropdown from "react-bootstrap/Dropdown";
-//import data from "../assets/autorsData";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Result from "./Result";
 import "../styles/Authors.css";
+import { Link } from "react-router-dom";
 
 function Authors() {
   const [authors, setAuthors] = useState([]);
+  const [search, setSearch] = useState();
+  const [songsByAuthor, setSongsByAuthor] = useState(null);
   useEffect(() => {
     fetch("http://127.0.0.1:5000/api/authors")
       .then((response) => response.json())
@@ -15,24 +19,52 @@ function Authors() {
   return (
     <div className="Authors">
       <h2>Lista de Autores</h2>
-      <div className="authorslist">
-        <ListGroup>
-          {authors &&
-            authors.map((author) => (
-              <ListGroup.Item key={author._id}>
-                <div className="container-list">
-                  <img src={author.image}></img>
-                  <div className="description">
-                    <h3>
-                      <em>{author.name}</em>
-                    </h3>
-                    <p>Canciones agregadas: {author.songs}</p>
-                  </div>
-                </div>
-              </ListGroup.Item>
-            ))}
-        </ListGroup>
-      </div>
+      <Form className="d-flex SongsSearch">
+        <Form.Control
+          onChange={(event) => {
+            setSearch(event.target.value);
+          }}
+          type="search"
+          placeholder="Search"
+          className="me-2"
+          aria-label="Search"
+        />
+        <Button
+          className="btn-search"
+          onClick={() => {
+            fetch(`http://127.0.0.1:5000/api/search?author=${search}`)
+              .then((response) => response.json())
+              .then((json) => setSongsByAuthor(json))
+              .catch((err) => console.log(err));
+          }}
+        >
+          Search
+        </Button>
+      </Form>
+      {songsByAuthor ? (
+        songsByAuthor && <Result result={songsByAuthor} />
+      ) : (
+        <div className="authorslist">
+          <ListGroup>
+            {authors &&
+              authors.map((author) => (
+                <Link to={`/authors/${author._id}`}>
+                  <ListGroup.Item key={author._id}>
+                    <div className="container-list">
+                      <img src={author.image}></img>
+                      <div className="description">
+                        <h3>
+                          <em>{author.name}</em>
+                        </h3>
+                        <p>Canciones agregadas: {author.songs}</p>
+                      </div>
+                    </div>
+                  </ListGroup.Item>
+                </Link>
+              ))}
+          </ListGroup>
+        </div>
+      )}
     </div>
   );
 }
